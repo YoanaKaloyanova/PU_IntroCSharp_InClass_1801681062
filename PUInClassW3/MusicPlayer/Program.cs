65568b9a0908a09a27228627ee7f9edbc1847241
yoana kaloianova <yoanak99@gmail.com>
2018-10-16 18:36:20 +0300

    Message

--- PUInClassW3/MusicPlayer/Program.cs
+++ PUInClassW3/MusicPlayer/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
+
 
 namespace MusicPlayer
 {
@@ -10,8 +12,27 @@ namespace MusicPlayer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello1");
+            Console.WriteLine("Do you want to play some music?");
+            string answer = Console.ReadLine();
             
+            if (answer == "yes")
+            {
+                Console.WriteLine("Playing music...!!");
+                
+                    using (SoundPlayer player = new SoundPlayer(@"C:\Users\Joana\Desktop\weeks\PU_IntroCSharp_InClass_1801681062\music\Official_Video_Clip-oal-MQkUoy8.wav"))
+                    {
+                        player.PlaySync();
+                    }
+            }
+            else if (answer == "no")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong Input.");
+            }
+            //Console.WriteLine("My answer is:{answer}"); 
         }
     }
 }
